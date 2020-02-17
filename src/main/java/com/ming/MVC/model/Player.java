49a09fdac5ce36239b0a0c5c19782cc26cc1f084
Ming <1284604307@qq.com>
2020-02-17 22:38:31 +0800

    炸弹逻辑重构，页面路由

--- src/main/java/com/ming/MVC/model/Player.java
+++ src/main/java/com/ming/MVC/model/Player.java
@@ -1,5 +1,6 @@
-package com.ming.MVC.model;
+package org.ming.model.players;
 
+import com.ming.MVC.controller.KeyStack;
 import com.ming.MVC.model.Prop.Prop;
 import com.ming.MVC.model.playerForms.骑士;
 import javafx.scene.canvas.GraphicsContext;
@@ -21,48 +22,53 @@ import static com.ming.MVC.model.data.BaseData.DOWN;
 
 public class Player extends Item implements RunBox, AttackObject<Bubble> , Serializable {
 
-
-    protected Image[] images;
-
     private final static double tranlateX = 4;
     private final static double translateY = 19;
+
 
     protected final static double centerX = 20;
     protected final static double centerY = 39;
 
+    private transient GameObjectManager gameObjectManager ;
+    private transient KeyStack<KeyCode> keyStack = new KeyStack<>();
+    protected TransStatus transStatus = TransStatus.NORMAL;
+    protected transient boolean canRun = true;
+
+    protected PlayerStatus playerStatus = PlayerStatus.STOPED;
     //无敌时间计数
     protected int InvincibleTimer = 0;
     protected boolean Invincible = true;
 
-    protected PlayerStatus playerStatus = PlayerStatus.NORMAL;
+    protected transient int drawTimer = 0;
 
-    protected boolean isRun = false;
-    protected boolean canRun = true;
-    //困住状态
-    protected boolean Trapped = false;
 
-    protected int runTimes = 0;
+
     protected byte dir = 0; // 上1 下0 左2 右3
     private int power = 1;
-
+    protected int speed = 3;
     private int bubbles = 2;
     private int putBubbles = 0;
 
-    public int getPower(){return power;}
-    public int getId(){return 1;}
 
-    protected int speed = 3;
+    public int getId(){return 0;}
 
     public Player(Image[] images){
         this.point = new Point(0,0);
         this.position = new Position(0,0);
-        this.images = images;
     }
 
-    public Player(){
+    public Player(GameObjectManager gameObjectManager){
         this.point = new Point(0,0);
         this.position = new Position(0,0);
-        this.images = images;
+        this.gameObjectManager = gameObjectManager;
+    }
+
+    public KeyStack<KeyCode> getKeyStack() {
+        return keyStack;
+    }
+
+    public GameObjectManager getGameObjectManager() {
+        return gameObjectManager;
     }
 
     public void initProperty(Player player){
@@ -71,12 +77,11 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
         this.bubbles = player.bubbles;
         this.point = player.point;
 
-        this.isRun = player.isRun;
         this.canRun = player.canRun;
-        this.Trapped = player.Trapped;
         this.dir = player.dir;
         this.dead = player.dead;
         this.recycled = player.recycled;
+
 
         if (point.x>14)  point.x = 14;
         else if (point.x<0) point.x = 0;
@@ -86,7 +91,6 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
         this.position = player.position;
         this.Invincible = true;
         this.InvincibleTimer = 0;
-        this.playerStatus = PlayerStatus.NORMAL;
     }
 
     public void updateProperty(Player player){
@@ -94,22 +98,31 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
         this.power = player.power;
         this.bubbles = player.bubbles;
         this.point = player.point;
-
-        this.isRun = player.isRun;
-        this.canRun = player.canRun;
-        this.Trapped = player.Trapped;
         this.dir = player.dir;
         this.dead = player.dead;
         this.recycled = player.recycled;
-
         this.position = player.position;
         this.Invincible = player.Invincible;
         this.InvincibleTimer = player.InvincibleTimer;
         this.playerStatus = player.playerStatus;
+        this.transStatus = player.transStatus;
+
     }
+
+    public PlayerStatus getPlayerStatus() {
+        return playerStatus;
+    }
+
+    public void setPlayerStatus(PlayerStatus playerStatus) {
+        this.playerStatus = playerStatus;
+    }
+
+    public TransStatus getTransStatus() {
+        return transStatus;
+    }
+
     @Override
     public void run(KeyCode key) {
-        isRun = true;
         switch (key){
             case LEFT:
                 dir = LEFT;
@@ -124,8 +137,10 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
                 dir = DOWN;
                 break;
             default:
-                isRun = false;
+                playerStatus = PlayerStatus.STOPED;
+                return;
         }
+        setPlayerStatus(PlayerStatus.RUNNING);
 
 
     }
@@ -135,18 +150,8 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
             putBubbles--;
     }
 
-    public boolean applyBubble(){
-        if (bubbles>putBubbles) {
-            putBubbles++;
-            return true;
-        }
-        return false;
-    }
-
-
     public boolean canAttack(){
-        if (playerStatus == PlayerStatus.a可达鸭) return false;
-        return putBubbles<bubbles&&(!Trapped&&!dead);
+        return putBubbles<getBubbles()&&(playerStatus==PlayerStatus.RUNNING||playerStatus==PlayerStatus.STOPED);
     }
 
     @Override
@@ -158,26 +163,41 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
         return null;
     }
 
-    public void canRun(ArrayList<Item>... itemsArr){
+    public void canRun(){
+
+        ArrayList<Item>[][] items = gameObjectManager.getItemsArr();
+
+        // 遵循规则: 下 上 左 右 中
+        //noinspection unchecked
+        ArrayList<Item>[] itemsArr = new ArrayList[]
+        {
+            getPoint().y < 12 ? items[getPoint().y + 1][getPoint().x] : null,
+            getPoint().y > 0 ? items[getPoint().y - 1][getPoint().x] : null,
+            getPoint().x > 0 ? items[getPoint().y][getPoint().x - 1] : null,
+            getPoint().x < 14 ? items[getPoint().y][getPoint().x + 1] : null,
+            items[getPoint().y][getPoint().x]
+        };
+
         // 0x04 中间位置 判断是否碰到炸弹爆炸
-        if (Trapped){
+        if (playerStatus==PlayerStatus.TRAPPED){
             // 待拯救状态
             canRun = false;
             if (itemsArr[CENTER]!=null)
                 for (Item item :
                         itemsArr[CENTER]) {
                     if (item.unitType == UnitType.EXPLODING) {
-                        Trapped = false;
+                        playerStatus = PlayerStatus.STOPED;
                         return;
                     }
                 }
-        }else {
+        }
+        else {
             if (itemsArr[CENTER]!=null)
                 for (Item item : itemsArr[CENTER]) {
                     if (item.unitType == UnitType.EXPLODING) {
                         // 正常状态(非无敌)
                         if (!Invincible)
-                            Trapped = true;
+                            playerStatus = PlayerStatus.TRAPPED;
                             return;
                     }
                     if(item instanceof Prop){
@@ -260,7 +280,7 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
                         if (item instanceof Prop){
                             //TODO
                             if (item.position.x  > this.position.x - 20){
-                                System.out.println(item.position.x +"  "+this.position.x);
+//                                System.out.println(item.position.x +"  "+this.position.x);
                                 enterGoods((Prop)item);
                             }
                             continue;
@@ -316,19 +336,16 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
     public void enterGoods(Prop prop){
         switch (prop.unitType){
             case 威力嘎嘎:
-                if (power<MaxPower)
-                    this.power++;
+                addPower(1);
                 break;
             case 糖炮多多:
-                if (bubbles<MaxBubbles)
-                    this.bubbles++;
+                addBubbles(1);
                 break;
             case 跑的快快:
-                if (speed<MaxSpeed)
-                    this.speed++;
+                addSpeed(1);
                 break;
             case KNIGHT_PROP:
-                this.playerStatus = PlayerStatus.a骑士;
+                this.transStatus = TransStatus.a骑士;
                 break;
         }
         prop.recycle();
@@ -341,62 +358,133 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
 
     @Override
     public void draw(GraphicsContext g) {
+        drawTimer = drawTimer++ % 3;
+        g.drawImage(ImageUrl.arrows.get(drawTimer),position.getX()+8,position.getY()-30);
 
-        g.fillRect(point.x*40,point.y*40,10,10);
 
+        switch (transStatus){
+            case NORMAL:
+                if (playerStatus == PlayerStatus.TRAPPED){
+                    //绘制 死亡/待拯救 状态
+                    if (!dead){
+                        g.fillText("\n\n我在这，\n快来救我",position.getX(),position.getY(),40);
+                    }
 
+                    g.drawImage(ImageUrl.Kngint[dir],position.x,position.y);
 
-        if (Trapped){
-            //绘制 死亡/待拯救 状态
-            if (!dead){
-                g.fillText("\n\n我在这，\n快来救我",position.getX(),position.getY(),40);
-            }
-        }else {
-            // 绘制人物行走状态
-            if (isRun){
-                //行走
-                if (runTimes>16)
-                    runTimes=0;
-                else
-                    runTimes ++;
+                    if (Invincible)
+                        g.drawImage(ImageUrl.InvincibleZero,position.x-3,position.y+5);
 
-                g.drawImage(this.images[runTimes/4%4+dir*4],position.getX(),position.getY());
-            }else {
-                g.drawImage(this.images[dir*4],position.getX(),position.getY());
-                runTimes=0;
-            }
+                }else {
+                    // 绘制人物行走状态
+                    if (playerStatus == PlayerStatus.RUNNING){
+                        //行走
+                        g.drawImage(ImageUrl.Monkey[dir],position.getX(),position.getY());
+                    }else {
+                        g.drawImage(ImageUrl.Monkey[dir+4],position.getX(),position.getY());
+                    }
+                }
+                break;
+            case a骑士:
+                g.drawImage(ImageUrl.Kngint[dir],position.x,position.y);
+                break;
         }
+
         if (Invincible) {
             g.drawImage(ImageUrl.InvincibleZero,position.x-3,position.y-5);
-
         }
     }
 
 
+    public void addPower(int power) {
+        if (power<MaxPower)
+        this.power += power;
+    }
+
+    public void addSpeed(int speed) {
+        if (speed<MaxSpeed)
+        this.speed += speed;
+    }
+
+    public int getPower(){
+        switch (transStatus){
+            case NORMAL:
+            case NULL:
+            case a泡泡皇后:
+            case a可达鸭:
+            case a骑士:
+                return power;
+            case a反身怪:
+                return MaxPower;
+        }
+        return power;
+    }
+
+    public int getSpeed() {
+        switch (transStatus){
+            case NORMAL:
+            case NULL:
+            case a泡泡皇后:
+            case a可达鸭:
+                return speed;
+            case a反身怪:
+            case a骑士:
+                return MaxSpeed;
+        }
+
+        return speed;
+    }
+
+    public int getBubbles() {
+        switch (transStatus){
+            case NORMAL:
+            case a骑士:
+            case NULL:
+                return bubbles;
+            case a反身怪:
+            case a泡泡皇后:
+                return MaxBubbles;
+            case a可达鸭:
+                return 0;
+        }
+        return bubbles;
+    }
+
+    public void addBubbles(int bubbles) {
+        if (bubbles<MaxBubbles)
+        this.bubbles += bubbles;
+    }
 
     @Override
     public void update() {
 
-
         //无敌
-        if (InvincibleTimer<2000){
-            InvincibleTimer+=0x0f;
+        if (InvincibleTimer<180){
+            InvincibleTimer+=1;
         }else Invincible = false;
 
-        if (isRun){
+        //todo 方向更新
+        if (!keyStack.isEmpty()) {
+            run(keyStack.peek());
+        }
+        else run(KeyCode.STOP);
+
+        canRun();
+
+        if (playerStatus == PlayerStatus.RUNNING){
             if (canRun){
                 switch (dir){
                     case LEFT:
-                        this.position.translateX(-speed);
+                        this.position.translateX(-getSpeed());
                         break;
                     case RIGHT:
-                        this.position.translateX(speed);
+                        this.position.translateX(getSpeed());
                         break;
                     case UP:
-                        this.position.translateY(-speed);
+                        this.position.translateY(-getSpeed());
                         break;
                     case DOWN:
-                        this.position.translateY(speed);
+                        this.position.translateY(getSpeed());
                         break;
                 }
             }
@@ -416,11 +504,9 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
     }
 
     public Player trans(){
-        switch (playerStatus){
+        switch (transStatus){
             case a骑士:
                 return new 骑士(this);
-            case NORMAL:
-                return this;
         }
         return this;
     }
@@ -428,26 +514,28 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("{");
-        sb.append("\"Invincible\":")
-                .append(Invincible);
-        sb.append(",\"position\":")
-                .append(position);
-        sb.append(",\"isRun\":")
-                .append(isRun);
+        sb.append("\"transStatus\":")
+                .append(transStatus);
         sb.append(",\"canRun\":")
                 .append(canRun);
-        sb.append(",\"Trapped\":")
-                .append(Trapped);
+        sb.append(",\"playerStatus\":")
+                .append(playerStatus);
+        sb.append(",\"InvincibleTimer\":")
+                .append(InvincibleTimer);
+        sb.append(",\"Invincible\":")
+                .append(Invincible);
+        sb.append(",\"drawTimer\":")
+                .append(drawTimer);
         sb.append(",\"dir\":")
                 .append(dir);
         sb.append(",\"power\":")
                 .append(power);
+        sb.append(",\"speed\":")
+                .append(speed);
         sb.append(",\"bubbles\":")
                 .append(bubbles);
         sb.append(",\"putBubbles\":")
                 .append(putBubbles);
-        sb.append(",\"speed\":")
-                .append(speed);
         sb.append(",\"dead\":")
                 .append(dead);
         sb.append(",\"recycled\":")
@@ -456,6 +544,8 @@ public class Player extends Item implements RunBox, AttackObject<Bubble> , Seria
                 .append(unitType);
         sb.append(",\"point\":")
                 .append(point);
+        sb.append(",\"position\":")
+                .append(position);
         sb.append('}');
         return sb.toString();
     }
